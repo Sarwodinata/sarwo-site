77605f1293f55586072097e62a01630e408b5f1b
Sarwo Dinata <sarwodinata07@gmail.com>
2025-11-07 22:51:06 +0700

    fix: package.json valid + next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 // next.config.ts
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // App Router sudah default di Next 16, jadi jangan pakai appDir lagi.
   experimental: {
-    tsconfigPaths: true, // biar alias "@/..." di-resolve saat build Vercel
-  },
+    tsconfigPaths: true
+  }
 };
 
 export default nextConfig;
